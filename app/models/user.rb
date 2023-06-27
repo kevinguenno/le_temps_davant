51df66d5d6b63387907db96d25fdba258ef14bb1
kevinguenno <kevin.guenno@gmail.com>
2023-06-27 16:55:53 +0200

    validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :order
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
